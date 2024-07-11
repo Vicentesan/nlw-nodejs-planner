945997349acf8a12c56836176009d9d49d7cb3f9
Vicente <vikom.sanchez@gmail.com>
2024-07-10 21:57:41 -0300

    feat(http/routes): improve json response on fetch activities route

--- src/routes/fetch-activities.ts
+++ src/routes/fetch-activities.ts
@@ -5,30 +5,63 @@ import { dayjs } from '../lib/dayjs'
 import { prisma } from '../lib/prisma'
 
 export async function fetchActivities(app: FastifyInstance) {
-  app.withTypeProvider<ZodTypeProvider>().get('/trips/:tripId/activities', {
-    schema: {
-      params: z.object({ tripId: z.string().uuid() }),
-      response: {
-        200: z.object({
-          activities: z.object({
-            id: z.string().uuid(),
-            title: z.string(),
-            occursAt: z.date(),
-          }).array()
-        })
-      }
+  app.withTypeProvider<ZodTypeProvider>().get(
+    '/trips/:tripId/activities',
+    {
+      schema: {
+        params: z.object({ tripId: z.string().uuid() }),
+        response: {
+          200: z.object({
+            activities: z
+            .object({
+              date: z.date(),
+              activities: z
+                .object({
+                  id: z.string().uuid(),
+                  title: z.string(),
+                  occursAt: z.date(),
+                })
+                .array(),
+            })
+            .array()
+          }),
+        },
+      },
+    },
+    async (req, res) => {
+      const { tripId } = req.params
+
+      const trip = await prisma.trip.findUnique({
+        where: { id: tripId },
+        include: {
+          activities: {
+            orderBy: { occursAt: 'asc' },
+          }
+        },
+      })
+
+      if (!trip) throw new Error('Trip not found')
+
+      const diffInDaysBetweenTripStartAndEndDates = dayjs(trip.endsAt).diff(
+        dayjs(trip.startsAt),
+        'days'
+      )
+
+      // + 1 because we want to include the start date
+      const activities = Array.from({
+        length: diffInDaysBetweenTripStartAndEndDates + 1,
+      }).map((_, i) => {
+        const date = dayjs(trip.startsAt).add(i, 'days')
+
+        return {
+          date: date.toDate(),
+          activities: trip.activities.filter((a) =>
+            dayjs(a.occursAt).isSame(date, 'day')
+          ),
+        }
+      })
+
+      return res.status(201).send({ activities })
     }
-  }, async (req, res) => {
-    const { tripId } = req.params
-
-    const trip = await prisma.trip.findUnique({
-      where: { id: tripId },
-      include: { activities: true }
-    })
-
-    if (!trip)
-      throw new Error('Trip not found')
-
-    return res.status(201).send({ activities: trip.activities })
-  })
+  )
 }
